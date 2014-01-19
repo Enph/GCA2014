0f3bdae321504ba453cea92a946889eb00ca8471
Ashan <ashan@ubuntu.(none)>
2014-01-19 16:36:16 -0500

    modified:   res/drawable-mdpi/frame.png 	new file:   res/drawable-mdpi/textquit.png

--- src/com/example/gca2014/Panel.java
+++ src/com/example/gca2014/Panel.java
@@ -369,6 +369,36 @@ SurfaceHolder.Callback {
 					else
 					{
 						if(startX==8&&startY==0){
+							this.context.mRenderer.removeDrawable(this);
+							panel.context.mRenderer.addDrawable(new Drawable(){
+								@Override
+								public void draw(GL10 gl) {
+									float[] vertices = new float[]{
+											-0.075f*9f, -0.075f*5f,  0.006f,		// V1 - bottom left
+											-0.075f*9f,  0.075f*5f,  0.006f,		// V2 - top left
+											0.075f*9f, -0.075f*5f,  0.006f,		// V3 - bottom right
+											0.075f*9f,  0.075f*5f,  0.006f,		// V4 - top right
+
+									};
+									ByteBuffer byteBuffer = ByteBuffer.allocateDirect(vertices.length * 4);
+									byteBuffer.order(ByteOrder.nativeOrder());
+									FloatBuffer vertexBuffer = byteBuffer.asFloatBuffer();
+									vertexBuffer.clear();
+									vertexBuffer.put(vertices);
+									vertexBuffer.position(0);
+									gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
+									gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, vertices.length / 3);
+								}
+
+								@Override
+								public int textureIndex() {
+									return 0;
+								}
+
+								@Override
+								public int textureSize() {
+									return getNote();
+								}});
 							context.finish();
 						}
 						int dx = lyden.getX()-startX;
